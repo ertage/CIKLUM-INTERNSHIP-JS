a69285641f3efe1a0b37f710acd906af9b44f9fd
Anastasiia Shvetsova <shv.an.vol@gmail.com>
2016-07-12 16:18:23 +0300

    structure of project is done

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,8 @@
 // jQuery(document).ready(function (){
-// 	var headerMenuItem = $('header__menu-li');
+// 	var headerMenuItem = $('.header__menu-li');
 
 // 	headerMenuItem.has('ul').bind('click',function(){
-// 		&(this).addclass('open');
+// 		$(this).toggleClass('open');
 // 	})
 
 // });
